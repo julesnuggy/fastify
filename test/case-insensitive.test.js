c11cc40734fff5057433892932156ec9bd8ba393
julesnuggy <julesnuggy@gmail.com>
2020-10-25 12:05:35 +0000

    Enable query params case-insensitive when caseSensitive false (#2644)

--- test/case-insensitive.test.js
+++ test/case-insensitive.test.js
@@ -118,3 +118,32 @@ test('case insensitive (wildcard)', t => {
     })
   })
 })
+
+test('case insensitive query params', t => {
+  t.plan(4)
+
+  const fastify = Fastify({
+    caseSensitive: false
+  })
+  t.tearDown(fastify.close.bind(fastify))
+
+  fastify.get('/foo', (req, reply) => {
+    reply.send({ hello: 'world', query: req.query.test })
+  })
+
+  fastify.listen(0, err => {
+    t.error(err)
+
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port + '/foo?TeSt=CaseInsensitiveQuery'
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 200)
+      t.deepEqual(JSON.parse(body), {
+        hello: 'world',
+        query: 'caseinsensitivequery'
+      })
+    })
+  })
+})
